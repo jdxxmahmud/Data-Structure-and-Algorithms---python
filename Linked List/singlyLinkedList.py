df0336881a24d3354e49b62dd4231b0a244d1563
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2022-08-15 16:59:27 +0600

    Linked List added

---
+++ Linked List/singlyLinkedList.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, val_):
+        self.val = val_
+        self.next = None
+
+class LinkedList:
+    def insertNode(root, val_):
+        if root is None:
+            return Node(val_)
+        currentNode = root
+        while currentNode.next:
+            currentNode = currentNode.next
+        currentNode.next = Node(val_)
+        return root
+
+    def printNodes(root):
+        while root:
+            print(root.val)
+            root = root.next
+
+root = LinkedList.insertNode(None, 2)
+root = LinkedList.insertNode(root, 4)
+root = LinkedList.insertNode(root, 10)
+root = LinkedList.insertNode(root, 12)
+root = LinkedList.insertNode(root, -1)
+root = LinkedList.insertNode(root, 0)
+LinkedList.printNodes(root)
