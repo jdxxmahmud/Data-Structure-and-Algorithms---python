6008562ee1c3c1eb93f39c7186dc63d262ce994a
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-09-01 18:47:38 +0600

    insertion added

--- Linked List/singlyLinkedList/linkedList.py
+++ Linked List/singlyLinkedList/linkedList.py
@@ -8,8 +8,9 @@ class LinkedList:
     head = None
     tail = None
 
-    def __init__(self, val):
-        self.head = Node(val)
+    def __init__(self, val=None):
+        # if no value is given, an empty linked list will be initialized
+        self.head = Node(val) if val else None
         self.tail = self.head
 
     def printList(self):
@@ -43,42 +44,56 @@ class LinkedList:
         return currentLength
 
     def insert(self, position, val):
-        if position == 1:
-            newNode = Node(val)
-            newNode.next = self.head
-            self.head = newNode
-            return
-        if self.head is not None:
+        if position < 0:
+            print("Index starts at 0")
+        elif position > self.findLength():
+            print("Exceeds index limit")
+        elif position == 1:
+            return self.insertFirst(val)
+        elif position == self.findLength():
+            return self.append(val)
+        else:
             currentNode = self.head
             currentPosition = 0
-            while currentNode is not None:
-                if currentPosition == position - 2:
-                    newNode = Node(val)
+
+            # This loop will run upto the previous node of the
+            #   expected position
+            while currentNode.next:
+                newNode = Node(val)
+                currentNode = currentNode.next
+                currentPosition += 1
+                if currentPosition == position - 1:
                     newNode.next = currentNode.next
                     currentNode.next = newNode
-
                     return
 
-                currentPosition += 1
-                currentNode = currentNode.next
+    def pop(self):
 
-            print("Not that many elements, so appending")
-            self.append(val)
+        prevNode = self.head
+
+        if self.head is self.tail:
+            self.head = self.tail = None
+            return prevNode
+        currentNode = self.head
+
+        while currentNode.next:
+            prevNode = currentNode
+            currentNode = currentNode.next
+
+        prevNode.next = None
+        self.tail = prevNode
+
+        return currentNode
+
+    def insertFirst(self, val):
+        newNode = Node(val)
+
+        if self.head is None:
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+
+        self.head = newNode
 
 
-myLL = LinkedList(5)
-myLL.append(6)
-print("Tail: ", myLL.tail.val)
-myLL.append(10)
-print("Tail: ", myLL.tail.val)
-myLL.printList()
-myLL.insert(3, 20)
-myLL.printList()
-myLL.insert(100, 3)
-myLL.printList()
-print("Tail: ", myLL.tail.val)
-myLL.insert(1, 125)
-myLL.printList()
-print("Head: ", myLL.head.val)
-
-print(f"Current length of linked list is: {myLL.findLength()}")
+myLL = LinkedList()
